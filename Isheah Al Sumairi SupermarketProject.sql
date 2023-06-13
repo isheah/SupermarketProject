a31076b2e4c74e4e23ca4f6aab68ab24db3c28da
Isheah Al Sumairi <135665314+isheah@users.noreply.github.com>
2023-06-13 15:47:47 -0700

    Add files via upload

---
+++ Isheah Al Sumairi SupermarketProject.sql
@@ -0,0 +1,36 @@
+SELECT *
+FROM SupermarketProject..SupermarketData
+
+-- Fashion accessories, Food and beverages, Electronic accessories, Sports and travel, Home and lifestyle, Health and beauty --
+SELECT Product_line, COUNT(Product_line)
+FROM SupermarketProject..SupermarketData
+GROUP BY Product_line
+ORDER BY COUNT(Product_line) DESC
+
+-- Yangon had the most male buyers and least female buyers and Naypitaw had the most female buyers and the least male buyers -- 
+SELECT City, COUNT(City) AS CountCity, Gender, COUNT(Gender) AS CountGender
+FROM SupermarketProject..SupermarketData
+GROUP BY City, Gender
+ORDER BY COUNT(City) DESC
+
+-- Gross income highest with cash and lowest with Ewallet --
+SELECT Payment, COUNT(Payment) AS CountPayment, AVG(gross_income) AS GrossIncome, AVG(Rating) AS AvgRating
+FROM SupermarketProject..SupermarketData
+GROUP BY Payment
+ORDER BY AVG(gross_income) DESC
+
+-- Avg rating highest with credit card and lowest with Ewallet --
+SELECT Payment, COUNT(Payment) AS CountPayment, AVG(gross_income) AS GrossIncome, AVG(Rating) AS AvgRating
+FROM SupermarketProject..SupermarketData
+GROUP BY Payment
+ORDER BY AVG(Rating) DESC
+
+--SELECT Product_line, COUNT(Product_line) AS CountProduct, Quantity, AVG(Quantity) AS AVGQuantity
+--FROM SupermarketProject..SupermarketData
+--GROUP BY Product_line, Quantity
+--ORDER BY AVG(Quantity) DESC
+
+
+
+
+
